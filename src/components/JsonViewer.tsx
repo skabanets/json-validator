699ebac99d0feaf02c8e821b3437dbe512dd99e8
Sergey Kabanets <skabanets97@gmail.com>
2025-06-19 03:25:28 +0300

    Add icons for status messages

--- src/components/JsonViewer.tsx
+++ src/components/JsonViewer.tsx
@@ -2,18 +2,26 @@ import type { FC } from "react";
 import { JsonView, allExpanded, defaultStyles } from "react-json-view-lite";
 import "react-json-view-lite/dist/index.css";
 import type { TJsonViewerProps } from "../models/editor.model";
+import { AlertCircle, FileJson } from "lucide-react";
 
 const JsonViewer: FC<TJsonViewerProps> = ({ parsedJson }) => {
   if (!parsedJson) {
     return (
       <div className="p-4 bg-gray-100 border rounded text-gray-500">
-        No valid JSON to display
+        <div className="flex gap-2.5">
+          <AlertCircle className="text-gray-500" />
+          <p>No valid JSON to display</p>
+        </div>
       </div>
     );
   }
 
   return (
     <div className="p-4 bg-white border rounded overflow-auto h-full">
+      <div className="flex items-center gap-2 mb-2">
+        <FileJson className="text-blue-500" />
+        <span className="font-bold">Valid JSON Preview:</span>
+      </div>
       <JsonView
         data={parsedJson}
         shouldExpandNode={allExpanded}
--- src/components/ValidationPanel.tsx
+++ src/components/ValidationPanel.tsx
@@ -1,10 +1,12 @@
 import type { FC } from "react";
 import type { TValidationPanelProps } from "../models/editor.model";
+import { CheckCircle, XCircle } from "lucide-react";
 
 const ValidationPanel: FC<TValidationPanelProps> = ({ errors }) => {
   if (errors.length === 0) {
     return (
-      <div className="p-4 bg-green-100 border border-green-400 rounded">
+      <div className="p-4 bg-green-100 border border-green-400 rounded flex gap-2.5">
+        <CheckCircle className="text-green-500" />
         JSON is valid
       </div>
     );
@@ -12,7 +14,10 @@ const ValidationPanel: FC<TValidationPanelProps> = ({ errors }) => {
 
   return (
     <div className="p-4 bg-red-100 border border-red-400 rounded overflow-auto max-h-48">
-      <h2 className="font-bold mb-2">Validation Errors:</h2>
+      <div className="flex gap-2.5">
+        <XCircle className="text-red-500" />
+        <h2 className="font-bold mb-2">Validation Errors:</h2>
+      </div>
       <ul className="list-disc pl-5 space-y-1">
         {errors.map((error, index) => (
           <li key={index} className="text-red-700">
